499f49849e38cdefa7018db3b2444256c7d2b8c7
nachogarcia1 <igarcia1@mail.austral.edu.ar>
2024-11-06 00:00:43 -0300

    progreso4

--- src/main/java/com/university/creators/Creator3.java
+++ src/main/java/com/university/creators/Creator3.java
@@ -1,23 +1,42 @@
-package com.university.creators;
-
-import com.university.evaluation.Evaluation;
-
-import java.util.List;
-
-public class Creator3 implements Creator {
-
-    private String filename;
-
-    public Creator3(String filename){
-        this.filename = filename;
-
-    }
-    @Override
-    public void create(){
-
-    }
-    @Override
-    public List<Evaluation> getdata(){
-        return null;
-    }
-}
+//package com.university.creators;
+//
+//import com.university.csv.CSVreader;
+//import com.university.evaluation.Evaluation;
+//
+//import java.util.List;
+//
+//public class Creator3 implements Creator {
+//
+//    private String filename;
+//
+//    public Creator3(String filename){
+//        this.filename = filename;
+//
+//    }
+//    @Override
+//    public void create(){
+//
+//        CSVreader csvreader = new CSVreader(filename);
+//        csvreader.read(",");
+//        List<String[]> allData = csvreader.getData();
+//
+//        for (String[] data : allData) {
+//            if (data.length != 0) {
+//
+//
+//               String SubjectName = data[0];
+//               String CriteriaType = data[1];
+//               String CriteriaValue = data[2];
+//               String EvaluationName = data[3::];
+//
+//            for (Course course: coursebystudents)
+//
+//
+//            }
+//
+//    }
+//    @Override
+//    public List<Evaluation> getdata(){
+//        return null;
+//    }
+//}
